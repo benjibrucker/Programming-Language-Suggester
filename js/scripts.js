87be7be2816e24f456d87bffb57a0a79bffe0ea6
BenjiBrucker <benjamin.brucker@gmail.com>
2021-01-30 23:05:03 -0600

    add 1st if statement in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var mostLiked = $("mostLiked").val();
 
-
+    if (mostLiked === "apple") {
+      $(#apple).show();
+      $(#python).hide();
+      $(#banking).hide;
+    }
+  })
 })
